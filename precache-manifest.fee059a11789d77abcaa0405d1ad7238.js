bb0c4e1a700daad9237330bef1fda1c49dbd594d
leqaa <leqaa.abujubbeh97@gmail.com>
2020-10-24 23:02:27 +0300

    Updates

---
+++ precache-manifest.fee059a11789d77abcaa0405d1ad7238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e465b23ec556a030922087f45214d0",
+    "url": "/project3/index.html"
+  },
+  {
+    "revision": "ea237b959ff66d46a4a1",
+    "url": "/project3/static/css/main.d7b840b3.chunk.css"
+  },
+  {
+    "revision": "02c3be7f9b5207e6c06e",
+    "url": "/project3/static/js/2.26442bc4.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/project3/static/js/2.26442bc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea237b959ff66d46a4a1",
+    "url": "/project3/static/js/main.61908f12.chunk.js"
+  },
+  {
+    "revision": "1d34258b16cb7272330d",
+    "url": "/project3/static/js/runtime-main.38e4df24.js"
+  }
+]);
